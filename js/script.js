e2b4f10d65cc9ec8b777a6743d64bce7abc282b2
TetraPost <kendarad@gmail.com>
2019-12-15 19:58:18 +0200

    fix revers and some check rules

--- js/script.js
+++ js/script.js
@@ -15,24 +15,62 @@ function statePosition() {
   return statePositionNow;
 }
 
-buttonRight.onclick = () => {
-  sp.classList.add('r');
-  if (sp.classList.contains('l')) {
-    sp.classList.remove('l');
-  }
-  console.log(sp);
-};
-
-
-buttonLeft.onclick = () => {
+function moveL() {
   sp.classList.add('l');
+  buttonLeft.classList.add('disabled');
+  buttonRight.classList.remove('disabled');
   if (sp.classList.contains('r')) {
     sp.classList.remove('r');
   }
   console.log(sp);
+}
+
+function moveR() {
+  sp.classList.add('r');
+  buttonRight.classList.add('disabled');
+  buttonLeft.classList.remove('disabled');
+  if (sp.classList.contains('l')) {
+    sp.classList.remove('l');
+  }
+  console.log(sp);
+}
+
+buttonLeft.onclick = () => {
+  if (sp.classList.contains('rotateR')) {
+    sp.classList.remove('rotateR');
+  }
+  const nowState = statePosition();
+  const check = sp.classList.contains('r');
+  if (check) {
+    sp.classList.remove('r');
+  } else {
+    sp.classList.remove('l');
+  }
+  sp.style.marginLeft = nowState;
+  sp.classList.add('rotateL');
+  setTimeout(moveL, 500);
+};
+
+buttonRight.onclick = () => {
+  if (sp.classList.contains('rotateL')) {
+    sp.classList.remove('rotateL');
+  }
+
+  const nowState = statePosition();
+  const check = sp.classList.contains('l');
+  if (check) {
+    sp.classList.remove('l');
+  } else {
+    sp.classList.remove('r');
+  }
+  sp.style.marginLeft = nowState;
+  sp.classList.add('rotateR');
+  setTimeout(moveR, 500);
 };
 
 buttonPaused.onclick = () => {
+  buttonRight.classList.remove('disabled');
+  buttonLeft.classList.remove('disabled');
   const nowState = statePosition();
   const check = sp.classList.contains('r');
   if (check) {
